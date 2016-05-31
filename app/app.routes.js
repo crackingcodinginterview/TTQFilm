7cdcec0e33f73eb5b3799020aa8599831e61d11b
vominhquoc <sliverwestlife4@gmail.com>
2016-05-31 11:22:30 +0700

    gan xong giao dien home

--- app/app.routes.js
+++ app/app.routes.js
@@ -4,10 +4,19 @@
 (function(){
     var app = angular.module('movieApp');
     app.config(function($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.otherwise("/home");
         $stateProvider
             .state('home', {
-                url : '/',
+                url : "/home",
                 templateUrl : 'app/components/home/homeView.html'
+            })
+            .state('login', {
+                url : '/login',
+                templateUrl : 'app/components/login/loginView.html'
+            })
+            .state('register', {
+                url : '/register',
+                templateUrl : 'app/components/register/registerView.html'
             });
     });
 }());
