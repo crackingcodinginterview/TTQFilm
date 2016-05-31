506f4fe686f489d638af315b7dd3a0a844b44f35
MTae <hmtri1011@gmail.com>
2016-06-01 00:48:05 +0700

    Update

--- TestComponents/js/loadSlider.js
+++ TestComponents/js/loadSlider.js
@@ -10,7 +10,18 @@ $(window).load(function() {
 
 	$('#secondary-slider').flexslider({
 		animation: "slide",
-		animationLoop: true, 
+		animationLoop: true,
+		itemWidth: 210,
+		animationSpeed: 1200,
+		slideshowSpeed: 7000,
+		controlNav: false,
+		itemMargin: 5,
+		controlsContainer: '.flex-container'
+	});
+
+	$('#third-slider').flexslider({
+		animation: "slide",
+		animationLoop: true,
 		itemWidth: 210,
 		controlNav: false,
 		itemMargin: 5,
--- TestComponents/js/script.js
+++ TestComponents/js/script.js
@@ -2,4 +2,10 @@ angular.module('myApp', ['film-module','ngAnimate','ngRoute','ngFx'])
 
 .controller('mainController', function(){
 	
+})
+
+.run(function($rootScope, $templateCache) {
+   $rootScope.$on('$viewContentLoaded', function() {
+      $templateCache.removeAll();
+   });
 });
