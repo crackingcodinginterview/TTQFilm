cc398bbd1d4998832e41dbb1f4516cbc96481e27
MTae <hmtri1011@gmail.com>
2016-06-02 16:42:31 +0700

    Update

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -2,22 +2,22 @@
 	var app = angular.module('movieApp');
 
 	app.controller('ListFilmController', function ($scope,$http) {
-		var ctrl = this;
 		window.sc = $scope;
 
-		ctrl.phimhanhdong = {};
-		ctrl.phimhoathinh = {};
+		sc.isLoading = true;
+		sc.phimhanhdong = {};
+		sc.phimhoathinh = {};
 
 		sc.getPhim = function (){
 			$http.get('https://api.myjson.com/bins/1pfn2').then(function(response){
-				ctrl.phimhanhdong = response.data;
+				sc.phimhanhdong = response.data;
 			});
 
 			$http.get('https://api.myjson.com/bins/1cse6').then(function(response){
-				ctrl.phimhoathinh = response.data;
+				sc.phimhoathinh = response.data;
 			});
-		}
 
+		}
 
 		sc.getPhim();
 	});
