c240270afe7da6f7985846ab18110b8cc20c9edd
vominhquoc <sliverwestlife4@gmail.com>
2016-06-02 16:47:04 +0700

    new update

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -16,12 +16,7 @@
 			$http.get('https://api.myjson.com/bins/1cse6').then(function(response){
 				sc.phimhoathinh = response.data;
 			});
-<<<<<<< HEAD
 		};
-=======
->>>>>>> cc398bbd1d4998832e41dbb1f4516cbc96481e27
-
-		}
 
 		sc.getPhim();
 	});
