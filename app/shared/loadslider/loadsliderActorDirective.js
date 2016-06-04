1f956fc77b43c000d97114d79eb6d619f0fbeac0
MTae <hmtri1011@gmail.com>
2016-06-04 14:40:16 +0700

    update giao dien xem phim

---
+++ app/shared/loadslider/loadsliderActorDirective.js
@@ -0,0 +1,28 @@
+(function(){
+	angular.module('movieApp').directive('sliderActor', function(){
+	// Runs during compile
+	return {
+		   restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+		   templateUrl: 'app/shared/loadslider/loadsliderActor.html',
+		   scope: {
+		   	animationType : '@',
+		   	listActor : '=',
+		   },
+
+		   link: function(scope, iElm, iAttrs) {
+
+		   	scope.$watch('listActor', function(newValue, oldValue) {
+		   		$('.flexslider').flexslider({
+		   			animation: scope.animationType,
+		   			animationLoop: true,
+		   			itemWidth: 125,
+		   			animationSpeed: 1200,
+		   			slideshowSpeed: 7000,
+		   			controlNav: false,
+		   			itemMargin: 5,
+		   		});
+		   	});	
+		   }
+		};
+	});
+}());
