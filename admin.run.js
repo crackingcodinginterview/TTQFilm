f56eec575edb89b56178686e4cd93e571cf1a1b2
milotiger <black.thanhtri@gmail.com>
2016-06-08 03:02:20 +0700

    roll back

--- admin.run.js
+++ admin.run.js
@@ -14,25 +14,25 @@
 		}
 		else console.log('valid cookie');
 
-		$interval(function () {
-			console.log('check cookie')
-			if ($cookies.getObject('user') == undefined)
-			{
-				blockUI.start("You are not log in!");
-				console.log('not loggin');
-			}
-			else if ($cookies.getObject('user').accountInfo.role != 'ADMIN')
-			{
-				blockUI.start("You are not an Admin!");
-				console.log('not admin');
-			}
-			else
-			{ 
-				console.log('valid cookie');
-				blockUI.stop();
-			}
-			$rootScope.$evalAsync();
-		},5000);
+		// $interval(function () {
+		// 	console.log('check cookie')
+		// 	if ($cookies.getObject('user') == undefined)
+		// 	{
+		// 		blockUI.start("You are not log in!");
+		// 		console.log('not loggin');
+		// 	}
+		// 	else if ($cookies.getObject('user').accountInfo.role != 'ADMIN')
+		// 	{
+		// 		blockUI.start("You are not an Admin!");
+		// 		console.log('not admin');
+		// 	}
+		// 	else
+		// 	{ 
+		// 		console.log('valid cookie');
+		// 		blockUI.stop();
+		// 	}
+		// 	$rootScope.$evalAsync();
+		// },5000);
 
 	});
 })();
